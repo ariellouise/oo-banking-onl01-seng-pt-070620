fdf957062b4f880f7e37cd43308ba28b4fe2c17a
ariel murphy <arielladders@gmail.com>
2020-08-04 01:51:23 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,14 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount 
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending" #part of attr accs. needs to be initialized 
+  end 
+  
+  def execute_transaction
+    
+  
 end
