33dec22a728edbe8043d5928ff524966ce8f5fdb
ariel murphy <arielladders@gmail.com>
2020-08-04 01:54:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -8,7 +8,19 @@ class Transfer
     @status = "pending" #part of attr accs. needs to be initialized 
   end 
   
+  def valid? 
+    self.sender.valid? && self.receiver.valid?
+  end 
   
-    
+  def execute_transaction
+    if valid? && @status == "pending" && sender.balance > amount
+      @sender.balance -= @amount 
+      @receiver.balance += @amount 
+      @status = "complete"
+    else 
+      @status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end 
+  end
   
 end
